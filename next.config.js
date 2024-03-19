3cd946b1a7f2fc0318aca53bcdbe56ca5ac7005b
Abhira Bhai <vasuraj5606@gmail.com>
2024-03-20 00:02:06 +0530

    action

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Add the following configuration
-  export: 'static', // Set export mode to static
-  // You can add other configurations here
-};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   // Add the following configuration
+//   export: 'static', // Set export mode to static
+//   // You can add other configurations here
+// };
 
-module.exports = nextConfig;
+// module.exports = nextConfig;
+const basePath = process.env.NODE_ENV === 'production' ? '/Next-Portfolio' : ''; // Adjust 'your-project-name'
+
+export default {
+  basePath,
+  export: 'static',
+  reactStrictMode: true,
+};
